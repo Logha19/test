94bbb48982745cf8e2a56009887b5f574415fae2
Logha19 <97751014+Logha19@users.noreply.github.com>
2022-01-29 15:52:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ function compute()
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
     var resultDisplay = document.getElementById("result");
-    resultDisplay.innerHTML = "<span class='text'>If you deposit" + "<span class='highlight'>" + principal + "</span>,"  +"</span>"  + " <br> <span class='text'>at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "</span>"  +"<br> <span class='text'>You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" +"</span>"  + ", <br><span class='text'> in the year " + "<span class='highlight'>" + yearNow + "</span>"+"</span>";
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
 function SliderValue()
 {
     var slider = document.getElementById("rate");
     var output = document.getElementById("rate_display");
-    output.innerHTML = slider.value; // Display the default slider value   
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
 }
